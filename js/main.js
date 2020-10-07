b374f4a40d9c172078d0eb472fb88d9b6ff2cfd7
Galina Popova <g89117301783@gmail.com>
2020-10-07 16:27:21 +0300

    Личный проект: больше деталей (часть 1). Исправила ошибки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-`use strict`;
-const photos = document.querySelector(`.pictures`);
+"use strict";
+
+const photosContainer = document.querySelector(`.pictures`);
 const photosTemplate = document.querySelector(`#picture`)
 	.content
   .querySelector(`.picture`);
@@ -8,20 +9,20 @@ const PHOTOS_AMOUNT = 25;
 
 const COMMENT_MESSAGES = [
   `Всё отлично!`,
-	`В целом всё неплохо. Но не всё.`,
-	`Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
-	`Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-	`Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
-	`Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
 ];
 
 const COMMENT_NAMES = [
-	`Артем`,
-	`Катя`,
-	`Вася`,
-	`Аня`,
-	`Петя`,
-	`Оля`
+  `Артем`,
+  `Катя`,
+  `Вася`,
+  `Аня`,
+  `Петя`,
+  `Оля`
 ];
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
@@ -29,27 +30,27 @@ const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + mi
 const getRandomFrom = (arr) => arr[getRandom(0, arr.length - 1)];
 
 const generateComments = (amount) => {
-	const comments = [];
-	for (let i = 0; i < amount; i++) {
-		comments.push({
-			avatar: `img/avatar-${getRandom(1,6)}.svg`,
-			message: getRandomFrom(COMMENT_MESSAGES),
-			name: getRandomFrom(COMMENT_NAMES)
-		});
-	}
-	return comments;
+  const comments = [];
+  for (let i = 0; i < amount; i++) {
+    comments.push({
+      avatar: `img/avatar - ${getRandom(1, 6)}.svg`,
+      message: getRandomFrom(COMMENT_MESSAGES),
+      name: getRandomFrom(COMMENT_NAMES)
+    });
+  }
+  return comments;
 };
 
 const generatePhotos = (amount) => {
-	const photos = [];
-	for (let i = 0; i < amount; i++) {
-		photos.push({
-			url: `photos/${i + 1}.jpg`,
-			description: `описание фотографии`,
-			likes: getRandom(15, 200),
-			comments: generateComments(getRandom(1, 6))
-		});
-	}
+  const photos = [];
+  for (let i = 0; i < amount; i++) {
+    photos.push({
+      url: `photos/${i + 1}.jpg`,
+      description: `описание фотографии`,
+      likes: getRandom(15, 200),
+      comments: generateComments(getRandom(1, 6))
+    });
+  }
   return photos;
 };
 
@@ -66,15 +67,13 @@ const renderPhoto = (photo) => {
 
 const renderAllPhotos = (photos) => {
   const photosFragment = document.createDocumentFragment();
-
   for (let i = 0; i < photos.length; i++) {
     photosFragment.appendChild(renderPhoto(photos[i]));
-  };
-
+  }
   return photosFragment;
-}
+};
 
 const mockPhotos = generatePhotos(PHOTOS_AMOUNT);
 
-photos.appendChild(renderAllPhotos(mockPhotos));
+photosContainer.appendChild(renderAllPhotos(mockPhotos));
 
